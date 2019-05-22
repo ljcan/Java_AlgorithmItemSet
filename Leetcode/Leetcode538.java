ac1d6a0cbc82a078b58750f3cb45e59faf02a972
刘军强 <2671268148@qq.com>
2019-05-22 10:50:03 +0800

    Convert BST to Greater Tree

---
+++ Leetcode/Leetcode538.java
@@ -0,0 +1,34 @@
+/**
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+          
+*/
+
+class Solution {
+    int sum=0;
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+    
+    public void convert(TreeNode node){
+        if(node==null){
+            return;
+        }
+        convert(node.right);
+        node.val+=sum;
+        sum=node.val;
+        convert(node.left);
+    }
+}
